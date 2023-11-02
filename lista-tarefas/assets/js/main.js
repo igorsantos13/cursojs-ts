4ff3d8c3a57b38b37a45ed88c4644f5af2e5155f
Igor Santos <igorsantos9513@outlook.com>
2023-11-02 16:57:28 -0300

    todo

--- lista-tarefas/assets/js/main.js
+++ lista-tarefas/assets/js/main.js
@@ -2,14 +2,19 @@ const botaoAddTarefa = document.querySelector('.btn-tarefa')
 const input = document.querySelector('.input-tarefa')
 const campoTarefas = document.querySelector('.tarefas')
 
-let inputArray = []
+let inputArray = {task: []}
 
+task = ['task 1', 'task2', 'task3']
 botaoAddTarefa.addEventListener('click', (e)=>{
-  let inputVal = input.value
   e.preventDefault()
-
-  inputArray.push(inputVal)
-  // console.log(inputArray)
+  //valor do input
+  let inputValue = input.value
+  //adicionar o valor em algo?
+  inputArray.task.push(inputValue)
+ 
+  inputArray.task.map(item => localStorage.setItem(item, item))
+  
+  
   pegarTarefa()
 })
 
@@ -19,14 +24,43 @@ function pegarTarefa(){
   let deleteButton = document.createElement('button')
   deleteButton.innerHTML = 'Apagar'
 
+  //deleta task
   deleteButton.addEventListener('click', (e)=> {
     const parentElement = e.target.parentElement
+    const itemToRemove = parentElement.firstChild.nodeValue
+    localStorage.removeItem(itemToRemove)
     parentElement.remove()
   })
 
-  inputArray.map(tarefa => li.innerHTML = tarefa)
+  //pega tasks do LS
+  const tasksFromLS = localStorage.getItem('task')
+  console.log(tasksFromLS)
 
+  //adiciona task no UL
+  inputArray.task.map(tarefa => li.innerHTML = tarefa)
+  
+  //adiciona delete button na tela
   li.appendChild(deleteButton)
-
+  
+  //adiciona LI
   campoTarefas.appendChild(li)
 }
+
+function getElementsFromLS(){
+  let tasksFromLS = []
+
+  for (let key in localStorage){
+    tasksFromLS = localStorage.getItem(key)
+ }
+
+ console.log(tasksFromLS)
+
+
+}
+
+// const getTasksFromLS = ()=> {
+//   localStorage.getItem("task")
+// } 
+// document.onload(getTasksFromLS)
+
+// console.log(getTasksFromLS)
