b34401c2f61fec2b6373821e626dfdfe3b7c3cb1
Igor Santos <igorsantos9513@outlook.com>
2024-01-09 17:27:05 -0300

    tststs

---
+++ cursoTS/index.js
@@ -0,0 +1,7 @@
+function greet(name) {
+    return "Ol\u00E1, ".concat(name);
+}
+console.log(greet('Lewis'));
+//para compilar código no terminal execute: npx tsc "nome do arquivo"
+
+const userName = 'John'
---
+++ cursoTS/index.ts
@@ -0,0 +1,16 @@
+function greet(name){
+  return `Olá, ${name}`;
+}
+
+console.log(greet('Lewis'));
+
+const userName = 'Peter'
+export default userName
+
+//para compilar código no terminal execute: npx tsc "nome do arquivo"
+//ao fazer isso se inicia o processo de compilação para a criação de um arquivo .js
+//o objetivo de ser executado pelo node
+
+
+//fun fact: caso seu código ts esteja escrito em js (o que obviamente nao é o recomendado)
+//o node consegue rodar o código sem problemas
